69c00e6bf241d1531249f11bb94dadeed859593d
ElBlaireau <elblaireau85@gmail.com>
2020-01-31 16:28:49 +0100

    Playing around with the PDF generation. Next step : Automate all the thing ! Other step : Do the todo's. Make the most beautiful todo ever !

--- download.py
+++ download.py
@@ -3,6 +3,7 @@
 # TODO : For the moment we are working directly in RAM. See if we don't consume too much resources.
 import requests
 import re
+from fpdf import FPDF, HTMLMixin
 from bs4 import BeautifulSoup
 from time import sleep
 
@@ -10,8 +11,8 @@ from time import sleep
 # Parameters for the URL base
 base_url = "https://www.fanfiction.net/s/"
 # TODO : Make it "cli" friendly !
-#id_fanfic = "6910226" # --> HP et les méthodes de la rationnalité pas utilisé pour tests.
-id_fanfic = "13096584" # --> Random pour fanfic pour test (15 chapitres, 103529 mots, autour de Killer Instinct)
+# id_fanfic = "6910226" # --> HP et les méthodes de la rationnalité pas utilisé pour tests.
+id_fanfic = "13096584"  # --> Random fanfic pour test (15 chapitres, 103529 mots, autour de Killer Instinct)
 # We build the full url, since we are going to need it !
 full_fanfic_url = base_url + id_fanfic
 # Regex in order to catch chapters name (awful hack done quick...) and cleaning some outputs
@@ -24,8 +25,13 @@ all_chapters = []
 # We don't want to stress the website. Let's sleep a little.
 time_sleep = 1
 
+
+class MyFPDF(FPDF, HTMLMixin):
+    pass
+
+
 # Doing the request ! TODO : Error catching
-print("Getting the page at : "+full_fanfic_url)
+print("Getting the page at : " + full_fanfic_url)
 requested_page = requests.get(full_fanfic_url)
 parsed_page = BeautifulSoup(requested_page.text, features="html.parser")
 print("Page got and parsed by BS")
@@ -51,11 +57,11 @@ for num_chap in chapters:
 # TODO : Maybe output in another format ? LaTex ? etc...
 # TODO : Check if the fanfic has only one chapter...
 
-print("Got "+str(len(chap_dict))+" chapters. Let's download that !")
+print("Got " + str(len(chap_dict)) + " chapters. Let's download that !")
 max_chapter = len(chap_dict)
 for key in chap_dict:
-    print("Getting chapter "+str(len(chap_dict)-max_chapter+1)+" on "+str(len(chap_dict)))
-    chapter_url = full_fanfic_url +'/'+key
+    print("Getting chapter " + str(len(chap_dict) - max_chapter + 1) + " on " + str(len(chap_dict)))
+    chapter_url = full_fanfic_url + '/' + key
     requested_page = requests.get(chapter_url)
     parsed_page = BeautifulSoup(requested_page.text, features="html.parser")
     # This line gets all the storytext, and convert it in a list. We will use this to build a "list of list" with all
@@ -68,13 +74,48 @@ for key in chap_dict:
 
 print("All chapters of fanfic retrieved ! Let's convert that in PDF !")
 
+print(type(all_chapters[0][1]))
+print(len(all_chapters[0][1]))
+
+print(all_chapters[0][1][1])
+print(type(all_chapters[0][1][1]))
+print(all_chapters[0][1][2])
+print(type(all_chapters[0][1][2]))
+
 temp_file = open("temp.txt", "w", encoding='utf-8')
 
 for i in range(len(all_chapters)):
-    temp_file.write('Title : '+str(all_chapters[i][0])+'\nContent : \n'+str(all_chapters[i][1]))
+    temp_file.write('Title : ' + str(all_chapters[i][0]) + '\nContent : \n' + str(all_chapters[i][1]))
     temp_file.write('\n\n')
 
 temp_file.close()
 
-#print(all_chapters)
-#print(chap_dict)
+# print(all_chapters)
+# print(chap_dict)
+
+my_pdf = MyFPDF()
+my_pdf.add_page()
+# TODO : Maybe some parametrization for the font ?
+# my_pdf.set_font("Arial", size=14)
+# my_pdf.cell(200,10,txt="Welcome to Python !",ln=1,align="C")
+print(all_chapters[0][1][0])
+html_temp = str(all_chapters[0][1][0]).replace("<strong>", "<B>").replace("</strong>", "</B>")
+print(html_temp)
+my_pdf.write_html(html_temp)
+
+print(all_chapters[0][1][2])
+html_temp = str(all_chapters[0][1][2]).replace("<strong>", "<B>").replace("</strong>", "</B>")
+print(html_temp)
+my_pdf.write_html(html_temp)
+
+print(all_chapters[0][1][3])
+html_temp = str(all_chapters[0][1][3]).replace("<strong>", "<B>").replace("</strong>", "</B>")
+print(html_temp)
+my_pdf.write_html(html_temp)
+
+print(all_chapters[0][1][8])
+html_temp = str(all_chapters[0][1][8]).replace("<strong>", "<B>").replace("</strong>", "</B>").replace("<em>", "<i>").replace("</em>", "</i>")
+print(html_temp)
+my_pdf.write_html(html_temp)
+
+my_pdf.output("testdemo.pdf", "F")
